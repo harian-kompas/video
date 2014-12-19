c6685b2b96c8acf7d8332c2c6df9eddad0f69cdc
Yosef Yudha Wijaya <semboyan35@gmail.com>
2014-12-19 20:20:15 +0700

    fixed some minor bugs in ios
    more ios6 bugs need attention

--- js/video.js
+++ js/video.js
@@ -5,6 +5,11 @@ var videos = (function () {
     var objects = {
             videoWrappers : document.getElementsByClassName('box-video')
         },
+        calculate = {
+            gcd : function (width, height) {
+                return (height === 0) ? width : calculate.gcd(height, width % height);
+            }
+        },
         humanReadableDuration = function (intDuration) {
             var h,
                 m,
@@ -24,10 +29,14 @@ var videos = (function () {
             return h + m + ':' + s;
         },
         setInitialDuration = function (barTime, video) {
-            var fn = function () {
-                barTime.textContent = humanReadableDuration(video.duration);
-            };
-            video.addEventListener('loadedmetadata', fn, false);
+            if (isNaN(video.duration)) {
+                barTime.textContent = '';
+            } else {
+                var fn = function () {
+                    barTime.textContent = humanReadableDuration(video.duration);
+                };
+                video.addEventListener('loadedmetadata', fn, false);
+            }
         },
         setProgress = function (progressCurrent, progressInput, video, btnPlay, barTime, controls) {
             var percentage,
@@ -212,8 +221,6 @@ var videos = (function () {
                 video = parent[i].getElementsByTagName('video')[0];
                 controls = parent[i].getElementsByClassName('controls')[0];
                 
-                video.setAttribute('width', parent[i].offsetWidth);
-                
                 icoLogo = document.createElement('div');
                 icoLogo.className = 'ico-logo';
                 
@@ -260,8 +267,13 @@ var videos = (function () {
                 controls.appendChild(btnPlay);
                 controls.appendChild(progressWrapper);
                 controls.appendChild(barTime);
-                controls.appendChild(volumeWrapper);
-                controls.appendChild(btnFullscreen);
+                if (!navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+                    //hide volume controls in Apple's products for audio's volume is controlled physically by users
+                    //hide fullscreen button also
+                    controls.appendChild(volumeWrapper);
+                    controls.appendChild(btnFullscreen);
+                }
+                
                 
                 setVolume(0.5, video);
                 togglePlay(btnPlay, btnPlay, video);
@@ -275,248 +287,34 @@ var videos = (function () {
                 setInitialDuration(barTime, video); //webkit returns NaN if video's duration value is fetched without listening to loadedmetadata event
             }
         },
+        setDimensions = function () {
+            var parent = objects.videoWrappers,
+                parentLen = parent.length,
+                i,
+                video,
+                rW = 16, //let's say the aspect ratio of all videos is 16:9
+                rH = 9,
+                vW,
+                vH;
+            
+            for (i = 0; i < parentLen; i++) {
+                video = parent[i].getElementsByTagName('video')[0];
+                
+                video.setAttribute('width', parent[i].offsetWidth);
+                video.setAttribute('height', parent[i].offsetWidth * (rH / rW));
+                video.style.width = parent[i].offsetWidth + 'px';
+                video.style.height = (parent[i].offsetWidth * (rH / rW)) + 'px';
+            }
+        },
         init = function () {
+            setDimensions();
             displayControls();
         };
     
-    return {init : init};
+    return {
+        init : init
+    };
 }());
 
-//var videos = (function () {
-//	'use strict';
-//	var iconsPath = './assets/img/',
-//        togglePlay = function (obj, button, video) {
-//
-//            var fn = function () {
-//                if (video.paused) {
-//                    video.play();
-//                    button.setAttribute('src', iconsPath + 'ico_pause.svg');
-//                } else {
-//                    video.pause();
-//                    button.setAttribute('src', iconsPath + 'ico_play.svg');
-//                }
-//            };
-//
-//            obj.addEventListener('click', fn, false);
-//        },
-//        toggleFullscreen = function (button, video) {
-//
-//            //var video = box.getElementsByTagName('video')[0];
-//            var fn = function () {
-//                if (!video.fullscreenElement && !video.mozFullScreenElement && !video.webkitFullscreenElement && !video.msFullscreenElement) {
-//                    if (video.webkitRequestFullscreen) {
-//                        video.webkitRequestFullscreen();
-//                    } else if (video.mozRequestFullScreen) {
-//                        video.mozRequestFullScreen();
-//                    } else if (video.msRequestFullscreen) {
-//                        video.msRequestFullscreen();
-//                    } else if (video.requestFullscreen) {
-//                        video.requestFullscreen();
-//                    }
-//                } else {
-//                    if (video.webkitExitFullscreen) {
-//                        video.webkitExitFullscreen();
-//                    } else if (video.mozCancelFullScreen) {
-//                        video.mozCancelFullScreen();
-//                    } else if (video.msExitFullscreen) {
-//                        video.msExitFullscreen();
-//                    } else if (video.exitFullscreen) {
-//                        video.exitFullscreen();
-//                    }
-//                }
-//            };
-//            button.addEventListener('click', fn, false);
-//        },
-//        toggleControls = function (video, controls) {
-//
-//            /*if (video.paused) {
-//                if (controls.classList.contains('hidden')) {
-//                    controls.classList.remove('hidden');
-//                }
-//            }*/
-//
-//            video.addEventListener('mouseover', function () {
-//                if (!video.paused) {
-//                    if (controls.classList.contains('hidden')) {
-//                        controls.classList.remove('hidden');
-//                    }
-//                }
-//
-//            }, false);
-//
-//            video.addEventListener('mouseout', function () {
-//                if (!video.paused) {
-//                    if (!controls.classList.contains('hidden')) {
-//                        controls.classList.add('hidden');
-//                    }
-//                }
-//            }, false);
-//
-//            controls.addEventListener('mouseover', function () {
-//                if (!video.paused) {
-//                    if (controls.classList.contains('hidden')) {
-//                        controls.classList.remove('hidden');
-//                    }
-//                }
-//
-//            }, false);
-//
-//            controls.addEventListener('mouseout', function () {
-//                if (!video.paused) {
-//                    if (!controls.classList.contains('hidden')) {
-//                        controls.classList.add('hidden');
-//                    }
-//                }
-//            }, false);
-//        },
-//        setProgress = function (obj, objCurrent, video, controls) {
-//
-//            var fn = function () {
-//                var percentage = Math.floor((100 / video.duration) * video.currentTime),
-//                    btnPlay = video.parentNode.getElementsByClassName('btn-play');
-//                obj.value = percentage;
-//                objCurrent.style.width = percentage + '%';
-//                if (video.ended) {
-//                    btnPlay[0].setAttribute('src', iconsPath + 'ico_play.svg');
-//                    if (controls.classList.contains('hidden')) {
-//                        controls.classList.remove('hidden');
-//                    }
-//                }
-//            };
-//
-//            video.addEventListener('timeupdate', fn, false);
-//        },
-//        setProgressChange = function (obj, video) {
-//
-//            var fn = function () {
-//                var time = video.duration * (obj.value / 100);
-//                video.currentTime = time;
-//            };
-//
-//            obj.addEventListener('change', fn, false);
-//        },
-//        setVolume = function (val, video) {
-//
-//            var vol = video.volume;
-//            vol += val;
-//
-//            if (vol >= 0 && vol <= 1) {
-//                video.volume = vol;
-//            } else {
-//                video.volume = (vol < 0) ? 0 : 1;
-//            }
-//        },
-//        setProgressVolume = function (obj, objCurrent, video, speakerIcon) {
-//
-//            var fn = function () {
-//                var vol = obj.value / 100;
-//                objCurrent.style.width = obj.value + '%';
-//                video.volume = vol;
-//                speakerIcon.className = (vol === 0) ? 'muted' : 'normal';
-//            };
-//
-//            obj.addEventListener('change', fn, false);
-//        },
-//        init = function () {
-//
-//            //declare variables first before jslint spank me
-//            var videoWrapper = document.getElementsByClassName('box-video'),
-//                vwLen = videoWrapper.length,
-//                i,
-//                video,
-//                controls,
-//                logo,
-//                btnFullscreen,
-//                btnPlay,
-//                progress,
-//                progressCurrent,
-//                volumeWrapper,
-//                volumeCurrent,
-//                volumeImg,
-//                volumeBar,
-//                w,
-//                h;
-//
-//            for (i = 0; i < vwLen; i++) {
-//                video = videoWrapper[i].getElementsByTagName('video')[0];
-//                controls = videoWrapper[i].getElementsByClassName('controls')[0];
-//                w = videoWrapper[i].offsetWidth;
-//                h = videoWrapper[i].offsetHeight;
-//                console.log('w: ' + w + ' h: ' + h);
-//                
-//                video.setAttribute('width', w);
-//                video.setAttribute('height', h);
-//                
-//                logo = document.createElement('img');
-//                logo.setAttribute('src', iconsPath + 'logo_kompas.svg');
-//                logo.setAttribute('alt', 'Kompas');
-//                logo.className = 'logo';
-//
-//                btnPlay = document.createElement('img');
-//                btnPlay.setAttribute('src', iconsPath + 'ico_play.svg');
-//                btnPlay.setAttribute('alt', 'Mainkan');
-//                btnPlay.setAttribute('title', 'Mainkan');
-//                btnPlay.className = 'btn-play';
-//                btnPlay.textContent = 'Main';
-//
-//                progress = document.createElement('input');
-//                progress.setAttribute('value', 0);
-//                progress.setAttribute('type', 'range');
-//                progress.className = 'bar-progress';
-//
-//                progressCurrent = document.createElement('div');
-//                progressCurrent.style.width = 0;
-//                progressCurrent.className = 'bar-progress-current';
-//
-//                //HTML5 video's volume has a default value of 1, therefore reduce it to 0.5
-//                setVolume(-0.5, video);
-//
-//                volumeWrapper = document.createElement('div');
-//                volumeWrapper.className = 'wrapper-volume';
-//                volumeCurrent = document.createElement('div');
-//                volumeCurrent.className = 'volume-current';
-//                volumeImg = document.createElement('img');
-//                volumeImg.className = 'normal';
-//                volumeImg.setAttribute('src', iconsPath + 'ico_mute.svg');
-//                volumeImg.setAttribute('alt', 'volume');
-//                volumeBar = document.createElement('input');
-//                volumeBar.setAttribute('value', (video.volume * 100));
-//                volumeBar.setAttribute('type', 'range');
-//                volumeBar.setAttribute('min', 0);
-//                volumeBar.setAttribute('max', 100);
-//                volumeBar.className = 'bar-volume';
-//
-//                volumeWrapper.appendChild(volumeCurrent);
-//                volumeWrapper.appendChild(volumeImg);
-//                volumeWrapper.appendChild(volumeBar);
-//
-//                btnFullscreen = document.createElement('img');
-//                btnFullscreen.setAttribute('src', iconsPath + 'ico_fullscreen.svg');
-//                btnFullscreen.setAttribute('alt', 'Layar penuh');
-//                btnFullscreen.setAttribute('title', 'Layar penuh');
-//
-//                btnFullscreen.className = 'btn-fullscreen';
-//
-//                controls.appendChild(logo);
-//                controls.appendChild(btnPlay);
-//                controls.appendChild(progressCurrent);
-//                controls.appendChild(progress);
-//                controls.appendChild(volumeWrapper);
-//                controls.appendChild(btnFullscreen);
-//
-//
-//                toggleControls(video, controls);
-//                togglePlay(video, btnPlay, video);
-//                togglePlay(btnPlay, btnPlay, video);
-//                setProgress(progress, progressCurrent, video, controls);
-//                setProgressChange(progress, video);
-//                setProgressVolume(volumeBar, volumeCurrent, video, volumeImg);
-//                toggleFullscreen(btnFullscreen, video);
-//
-//            }
-//        };
-//
-//    return {init : init};
-//}());
 
 window.addEventListener('load', videos.init, false);
